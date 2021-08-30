b1e164b2bdc21a2c99bec1f4a995ccb94ec1117d
TemaXTest <89482053+TemaXTest@users.noreply.github.com>
2021-08-30 23:14:28 +0300

    Update BuyDuck.java

--- BuyDuck/src/test/java/ru/litecart/stqa/BuyDuck.java
+++ BuyDuck/src/test/java/ru/litecart/stqa/BuyDuck.java
@@ -53,7 +53,7 @@ public class BuyDuck {
 
         WebElement cart = driver.findElementByXPath("//*[@id='box-product']/div[2]/div[2]/div[5]/form/table/tbody/tr/td/button");
         cart.sendKeys(Keys.ENTER);
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
         WebElement checkout = driver.findElementByXPath("//*[@id='cart']/a[3]");
         checkout.sendKeys(Keys.ENTER);
